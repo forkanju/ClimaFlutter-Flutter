e1eb6db91c4346fef3fd2413d2624fb017e741e7
forkanju <forkanju44rth@gmail.com>
2021-01-03 17:04:23 +0600

    first and final commit for clima test app

---
+++ lib/services/weather.dart
@@ -0,0 +1,58 @@
+import 'package:clima_test/services/location.dart';
+import 'package:clima_test/services/networking.dart';
+
+const apiKey = '162cf2ace3f8835a231f1b7f52fb02db';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async{
+
+
+    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔  ';
+    } else if (condition < 700) {
+      return '☃';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀  ';
+    } else if (condition <= 804) {
+      return '☁  ';
+    } else {
+      return '🤷';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 32) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 25) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 25) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
